db31ff88e1b73e645e7d5b8fe89c98f16671d8c9
jermspeaks <jermspeaks@gmail.com>
2017-06-30 10:24:23 -0700

    checked streaming works to dl

--- streaming-m3u8.sh
+++ streaming-m3u8.sh
@@ -1,3 +1,13 @@
 #/bin/bash
 
-ffmpeg -i $0 -c copy -bsf:a aac_adtstoasc $1
+echo $0, $1, $2, $3
+
+if [ "$1" == "" ]; then
+    echo "Need to give m3u8 file"
+    exit 1;
+elif [ "$2" == "" ]; then
+    echo "default output file to ./output.mp4"
+    ffmpeg -i $1 -c copy -bsf:a aac_adtstoasc ./output.mp4
+else
+    ffmpeg -i $1 -c copy -bsf:a aac_adtstoasc $2
+fi
